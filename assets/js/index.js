585e41bfabafa23193c0afa282ad74eeb42f3b6f
LESS14 <100519912+LESS14@users.noreply.github.com>
2023-05-04 02:03:46 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 function copy(link) {
-    var input = document.createElement('textarea'); 
-    document.body.appendChild(input); 
-    input.value = link; 
+    var input = document.createElement('textarea');
+    document.body.appendChild(input);
+    input.value = link;
     input.focus();
     input.select();
-    document.execCommand("copy"); 
+    document.execCommand("copy");
     input.remove();
     alert("Conteúdo copiado para a área de transferência!");
 }
